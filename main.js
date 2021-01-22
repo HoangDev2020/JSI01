fb1538773d82b916b9760f2e2d4e4cdc41f14226
Hoang <poho1234569@gmail.com>
2021-01-22 09:16:26 -0500

    homework

---
+++ main.js
@@ -0,0 +1,42 @@
+
+//function logVisibleLightWaves(){
+    //console.log(lightWaves); // --> Cannot access 'lightWaves' before initialization
+    //const lightWaves = 'Moonlight'
+    // console.log(lightWaves);
+//}
+//console.log(lightWaves); // --> lightWaves is not defined
+//logVisibleLightWaves()
+
+// let longestWord 
+// let findLongestWord = (word) => {
+//     let newWord = word.split(' ')
+//     for(let i = 0; i < newWord.length; i++){
+//         longestWord = newWord[i]
+//         for(let j = 0; j < newWord.length; j++){
+//             if(i != j){
+//                 if (newWord[i].length < newWord[j].length){
+//                     longestWord = newWord[j]
+//                 }
+//             }
+//         }
+//     }
+//     return longestWord
+// }
+
+// console.log(findLongestWord('mindx js intensive'));
+
+// let longestName
+// let longestCountryName = (name) => {
+//     for(let i = 0; i < name.length; i++){
+//         longestName = name[i]
+//         for(let j = 0; j < name.length; j++){
+//             if(i != j){
+//                 if(name[i].length < name[j].length){
+//                     longestName = name[j]
+//                 }
+//             }
+//         }
+//     }
+//     return longestName
+// }
+// console.log(longestCountryName(['vietnam', 'lao', 'chingchong']))
