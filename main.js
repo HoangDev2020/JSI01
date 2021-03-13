ad144cc4e9e4f841ede0071db3a943d629ba2d1b
Hoang <poho1234569@gmail.com>
2021-03-13 09:48:24 -0500

    answer too awsome

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
 //function logVisibleLightWaves(){
-    //console.log(lightWaves); // --> Cannot access 'lightWaves' before initialization
-    //const lightWaves = 'Moonlight'
-    // console.log(lightWaves);
+//console.log(lightWaves); // --> Cannot access 'lightWaves' before initialization
+//const lightWaves = 'Moonlight'
+// console.log(lightWaves);
 //}
 //console.log(lightWaves); // --> lightWaves is not defined
 //logVisibleLightWaves()
@@ -71,51 +71,130 @@
 // console.log(vietnam.xin_chao());
 
 
-class Book {
-    constructor(name, price, publishedDay) {
-        this.id = uuid.v4()
-        this.name = name
-        this.price = price
-        this.publishedDay = publishedDay
-    }
-    
-}
+// class Book {
+//     constructor(name, price, publishedDay) {
+//         this.id = uuid.v4()
+//         this.name = name
+//         this.price = price
+//         this.publishedDay = publishedDay
+//     }
 
-class BookShelf {
-    constructor(name, owner, dateModified){
-        this.name = name
-        this.owner = owner
-        this.date = dateModified
-        this.books = []
-    }
-    add(book) {
-        if(book instanceof Book) {
-            this.books.push(book)
-        }
-    }
-    updateBook(id, data) {
-        for(let book of this.books){
-            if(book.id === id){
-                book.update(data)
-                break
-            }
-        }
-    }
-    deleteBook(id) {
-        for(let i = 0; i < this.books.length; i++){
-            if(this.books[i].id = id){
-                this.books.splice(i, 1)
-            }
-        }
-    }
-    showBooks(name) {
-        for(let i = 0; i < this.books.length; i++){
-            if(this.books[i].name = name){
-                console.log(this.books[i].name);
-            }
-        }
-    }
-}
+// }
+
+// class BookShelf {
+//     constructor(name, owner, dateModified){
+//         this.name = name
+//         this.owner = owner
+//         this.date = dateModified
+//         this.books = []
+//     }
+// add(book) {
+//     if(book instanceof Book) {
+//         this.books.push(book)
+//     }
+//     }
+//     updateBook(id, data) {
+//         for(let book of this.books){
+//             if(book.id === id){
+//                 book.update(data)
+//                 break
+//             }
+//         }
+//     }
+//     deleteBook(id) {
+//         for(let i = 0; i < this.books.length; i++){
+//             if(this.books[i].id = id){
+//                 this.books.splice(i, 1)
+//             }
+//         }
+//     }
+//     showBooks(name) {
+//         for(let i = 0; i < this.books.length; i++){
+//             if(this.books[i].name = name){
+//                 console.log(this.books[i].name);
+//             }
+//         }
+//     }
+// }
 
 // Music : id song singer artist
 // VpopMusic, US Uk, Kpop, Libraries:owner, id, quantity, Views(tat ca thong tin), ViewDetail(ten cac bai hat)
+
+class Music {
+    constructor(song, singer, country) {
+        this.id = uuid.v4()
+        this.song = song
+        this.singer = singer
+        this.country = country
+        this.data = {
+            id: this.id,
+            song: this.song,
+            singer: this.singer,
+            country: this.country
+        }
+    }
+}
+
+
+
+class Vpop extends Music {
+    constructor(song, singer, country, rank) {
+        super(song, singer, country)
+        this.data.rank = rank
+    }
+}
+
+
+
+class USUK extends Music {
+    constructor(song, singer, country, rank) {
+        super(song, singer, country)
+        this.data.rank = rank
+    }
+}
+
+class Libraries {
+    constructor(owner) {
+        this.id = uuid.v4()
+        this.owner = owner
+        this.musics = []
+    }
+    add(data) {
+        if (data instanceof Music) {
+            this.musics.push(data)
+        }
+    }
+    views() {
+        console.log(musics);
+    }
+    viewSong() {
+        for (let i = 0; i < this.musics.length; i++) {
+            console.log(this.musics[i].song);
+        }
+    }
+    delete(id) {
+        for (let i = 0; i < this.musics.length; i++) {
+            if (this.musics[i].id = id) {
+                this.musics.splice(i, 1)
+            }
+        }
+    }
+    update(id, data) {
+        for (let i = 0; i < this.musics.length; i++) {
+            if (this.musics[i].id = id) {
+                this.musics.push(data)
+            }
+        }
+    }
+    viewRank(country) {
+        for (let i = 0; i < this.musics.length; i++) {
+            if (this.musics[i].country = country) {
+                console.log(this.data[i].song);
+                console.log(this.data[i].rank);
+            }
+        }
+    }
+}
+
+
+
